ed5b8e4ce2d0c14ec7ac7dda77de31f507b53f7f
emirba06 <barrionuevojuan305@gmail.com>
2026-03-15 16:50:12 -0300

    subo tp3

---
+++ index.mjs
@@ -0,0 +1,12 @@
+import { leersuperHeroes, agregarSuperheroes } from './utils.mjs';
+
+const archivoOriginal = './superheroes.txt';
+const archivoNuevos = './agregarSuperheroes.txt';
+
+// Agregar nuevos superhéroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
+
+// Leer y mostrar la lista actualizada de superhéroes ordenada
+const superheroes = leersuperHeroes(archivoOriginal);
+console.log('Superhéroes ordenados:');
+console.log(superheroes);
